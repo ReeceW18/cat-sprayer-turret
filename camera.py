c7ba7e22a5bd9d8e45a29302e1ef0bc07a8de98a
Reece Whitaker <whitaker.reece@gmail.com>
2025-11-11 14:08:35 -0500

    added configuration file

--- camera.py
+++ camera.py
@@ -1,7 +1,23 @@
 import imagezmq
 import cv2
 from picamera2 import Picamera2
+import configparser
+import os
 
+# pull config values
+config = configparser.ConfigParser()
+
+config_file_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+
+try:
+    config.read(config_file_path)
+except Exception as e:
+    print(f"Error reading config file: {e}\n Did you set up config file properly?")
+
+receiver_ip = config.get('NETWORK', 'RECEIVER_IP')
+receiver_port_str = config.get('NETWORK', 'RECEIVER_PORT')
+
+# initialize camera
 picam2 = Picamera2()
 picam2.preview_configuration.main.size = (240,240)
 picam2.preview_configuration.main.format = "RGB888"
@@ -9,14 +25,15 @@ picam2.preview_configuration.align()
 picam2.configure("preview")
 picam2.start()
 
-sender = imagezmq.ImageSender(connect_to='tcp://[REDACTED_IP]:5555')
+# initialize connection
+sender = imagezmq.ImageSender(connect_to='tcp://{receiver_ip}:{receiver_port_str}')
 
 rpi_name = "Pi_YOLO"
 
+# continuously send images
 while True:
     frame = picam2.capture_array()
     cv2.imwrite("image.png", frame)
 
     print("sending image...")
     sender.send_image(rpi_name, frame)
-
--- view-camera.py
+++ view-camera.py
@@ -1,8 +1,22 @@
 import imagezmq
 import cv2
+import configparser
+import os
+
+# pull config values
+config = configparser.ConfigParser()
+
+config_file_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+
+try:
+    config.read(config_file_path)
+except Exception as e:
+    print(f"Error reading config file: {e}\n Did you set up config file properly?")
+
+receiver_port_str = config.get('NETWORK', 'RECEIVER_PORT')
 
 # Initialize the ImageHub (server)
-image_hub = imagezmq.ImageHub('tcp://*:5555') 
+image_hub = imagezmq.ImageHub('tcp://*:{receiver_port_str}') 
 
 while True: 
     # Receive the sender's name and the frame
