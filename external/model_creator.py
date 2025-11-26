58e26467bf6c76dd650e2296c14116b98babfc39
ReecW18 <whitaker.reece@gmail.com>
2025-11-26 15:58:01 -0500

    add models

--- external/model_creator.py
+++ external/model_creator.py
@@ -3,19 +3,20 @@ from ultralytics import YOLO
 import os
 
 # settings
-source_model = "yolov8n.pt"
-output_name = "8n_320"
+source_model = "yolo11s.pt"
+output_name = "11s_320p_halfprecision"
 image_size = 320
 
 # create new model
 model = YOLO(source_model)
-export_path = model.export(format="ncnn", imgsz=image_size)
+export_path = model.export(format="ncnn", imgsz=image_size, half=True)
 
 # move model to final location
-final_path = "models/" + output_name + "_ncnn_model/" # NOTE must have _ncnn_model suffix for yolo to recognize the model
+models_path = "models/"
+final_path = models_path + output_name + "_ncnn_model/" # NOTE must have _ncnn_model suffix for yolo to recognize the model
 
-if not os.path.exists(final_path):
-    os.makedirs(final_path)
+if not os.path.exists(models_path):
+    os.makedirs(models_path)
 shutil.move(export_path, final_path)
 
-print(f"model exported to {final_path}/{export_path}")
+print(f"model exported to {final_path}")
