afc82c119fe8391a8e7ca75945019b2959ddab25
ReeceW18 <whitaker.reece@gmail.com>
2025-12-02 13:00:45 -0500

    rewrite capture and processing to improve handling of framerate and queues

--- core/threads.py
+++ core/threads.py
@@ -6,7 +6,6 @@ Usage:
     daemons except for video_saver
 
 TODO:
-    - potentially set fps in SystemState
     - implement hardware, and video saving
     - see individual function todos
 """
@@ -26,7 +25,7 @@ from core.state_manager import SystemMode, SystemState, ThreadingDeque
 from hardware.hardware_control import HardwareCommand
 from vision.camera import Camera
 
-def capture_frames(camera: Camera, raw_queue: queue.Queue, state: SystemState):
+def capture_frames(camera: Camera, raw_queue: queue.Queue, frame_history: ThreadingDeque, post_roll_queue: queue.Queue, state: SystemState):
     """
     Captures frames from camera and adds to queues.
 
@@ -38,29 +37,32 @@ def capture_frames(camera: Camera, raw_queue: queue.Queue, state: SystemState):
         system, vessel for all simple variables shared by threads
 
     TODO:
-        - Refine fps definition
-            - store constants in config, 
-            - should fps be set in systemstate? 
-            - should fps change at all just have processing thread decide what 
-            frames need to be processed?
-        - add to history/postroll in this thread instead of processing
     """
+    last_capture_time = time.time()
     while state.mode != SystemMode.SHUTDOWN:
-        # set fps based on mode
-        if state.mode == SystemMode.SENTRY:
-            FPS = 1
-        else:
-            FPS = 10
-
         frame = camera.capture()
 
-        # timestamp is used to sync detection data with frames
+        debug_capture_time = False
+        if debug_capture_time:
+            this_capture_time = time.time()
+            capture_fps = 1 / (this_capture_time - last_capture_time)
+            last_capture_time = this_capture_time
+            print(f"capture fps = {capture_fps}")
+
+        _, compressed_frame = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), config.compression.recording_compression])
         timestamp = time.time()
+
+        if state.mode == SystemMode.SENTRY or state.mode == SystemMode.AIMING:
+            frame_history.append((compressed_frame, timestamp))
+        elif state.mode == SystemMode.COOLDOWN:
+            post_roll_queue.put((compressed_frame, timestamp)) #TODO double check adding properly when queue is full and when video recorder resets everything. check for timeout?
 
         if not raw_queue.full():
             raw_queue.put((frame, timestamp))
+        
+        time.sleep(1/config.camera.fps_recording)
 
-        time.sleep(1/FPS)
+        
 
 def stream_frames(stream_queue: queue.Queue, state: SystemState):
     """
@@ -108,7 +110,7 @@ def stream_frames(stream_queue: queue.Queue, state: SystemState):
         # this blocks until image is received
         sender.send_image(rpi_name, compressed_frame)
 
-def yolo_processing(trigger_event: threading.Event, raw_queue: queue.Queue, stream_queue: queue.Queue, frame_history: ThreadingDeque, post_roll_queue: queue.Queue, metadata_queue: ThreadingDeque, hardware_command_queue: queue.Queue, state: SystemState):
+def yolo_processing(trigger_event: threading.Event, raw_queue: queue.Queue, stream_queue: queue.Queue, metadata_queue: ThreadingDeque, hardware_command_queue: queue.Queue, state: SystemState):
     """
     Run processing on images and make decisions for hardware commands
 
@@ -131,7 +133,6 @@ def yolo_processing(trigger_event: threading.Event, raw_queue: queue.Queue, stre
     TODO:
         - could make into a process to max speed, or even run yolo inference 
         only on its own process
-        - move video frames storage to capture thread
         - create new special hardware queue class to make sure queue doesn't fill up
             - on add, delete all aim commands but keep fire command
             - shouldn't need to change logic in this function
@@ -139,13 +140,17 @@ def yolo_processing(trigger_event: threading.Event, raw_queue: queue.Queue, stre
     object_detector = ObjectDetector()
     # used to determine processing fps for display when streaming frames
     last_frame_time = time.time()
+    trigger_time = -1
 
     while state.mode != SystemMode.SHUTDOWN:
         frame, timestamp = raw_queue.get()
 
         # if in a mode that needs inference to run
         if state.mode == SystemMode.SENTRY or state.mode == SystemMode.AIMING:
+            predict_start = time.time()
             results = object_detector.predict(frame)
+            predict_end = time.time()
+            latency = predict_end-predict_start
 
             if state.mode == SystemMode.SENTRY:
                 if results.has_target():
@@ -155,6 +160,7 @@ def yolo_processing(trigger_event: threading.Event, raw_queue: queue.Queue, stre
                 direction = results.get_direction()
                 if direction == TargetDirection.CENTER:
                     trigger_event.set()
+                    trigger_time = time.time()
                     hardware_command_queue.put(HardwareCommand.FIRE)
                     state.mode = SystemMode.COOLDOWN
                 elif direction == TargetDirection.LEFT:
@@ -163,8 +169,6 @@ def yolo_processing(trigger_event: threading.Event, raw_queue: queue.Queue, stre
                     hardware_command_queue.put(HardwareCommand.AIM_RIGHT)
 
             metadata_queue.append((results, timestamp))
-            _, compressed_frame = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
-            frame_history.append((compressed_frame, timestamp))
 
             # calculate fps
             current_time = time.time()
@@ -172,20 +176,26 @@ def yolo_processing(trigger_event: threading.Event, raw_queue: queue.Queue, stre
             fps = 1 / time_since_last_frame
             last_frame_time = current_time
 
-            print(f"FPS: {fps:.0f}")
+            # print(f"FPS: {fps:.1f}")
             
             if not stream_queue.full():
                 # generate and stream frame
                 processed_frame = object_detector.overlay(frame, results, state, fps)
                 stream_queue.put(processed_frame)
 
-        else:
-            if trigger_event.is_set():
-                post_roll_queue.put((frame, timestamp))
+            if state.mode == SystemMode.SENTRY:
+                time.sleep(1/config.camera.fps_sentry - latency)
             else:
-                # TODO implement waiting extra for systemstate.cooldowntime
+                sleep_time = 1/config.camera.fps_aiming - latency
+                if sleep_time > 0:
+                    time.sleep(sleep_time)
+            
+        else:
+            time_since_trigger = time.time() - trigger_time 
+            if not trigger_event.is_set() and time_since_trigger > config.durations.cooldown_min_seconds:
                 state.mode = SystemMode.SENTRY
-
+            else:
+                time.sleep(.1)
 
 def hardware_control(aim_motor, trigger_motor, hardware_command_queue, state: SystemState):
     """
